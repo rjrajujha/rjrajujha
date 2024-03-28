95629c945562a4289823975292feca1553165cbb
rjrajujha <rjrajujha@outlook.com>
2024-03-28 22:19:59 +0530

    portfolio init

--- index.php
+++ index.php
@@ -5,16 +5,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Developer Portfolio</title>
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
-
     <?php include 'includes/header.php'; ?>
 
-    <!-- Your content goes here -->
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+                <h2 class="mb-4">About Me</h2>
+                <p class="lead">
+                    As a software developer, I specialize in JavaScript with a focus on Node.js and its associated frameworks and libraries such as Express.js, React.js, and Next.js. I also have proficiency in Python, primarily for solving algorithmic challenges and data structures. Although my expertise lies in backend and frontend development, I also possess a working knowledge of PHP.
+                </p>
+                <p>
+                    Throughout my career, I've gained valuable experience working with various databases, including MongoDB and MySQL. I'm passionate about creating efficient and scalable solutions, and I thrive in dynamic and collaborative environments where I can continuously learn and grow.
+                </p>
+            </div>
+        </div>
+    </div>
 
+    <?php include 'includes/footer.php'; ?>
     <!-- Bootstrap JS and dependencies -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>

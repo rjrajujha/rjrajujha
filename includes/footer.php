d56e36fea788bf515760859cbff86f7b1df37b91
rjrajujha <rjrajujha@outlook.com>
2024-03-28 22:23:20 +0530

    auther name updated

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
  <footer class="footer mt-auto py-3 bg-light">
      <div class="container text-center">
-         <span class="text-muted">© 2024 Your Name. All rights reserved.</span>
+         <span class="text-muted">&copy; 2024 Raju Jha. All rights reserved.</span>
      </div>
  </footer>

5b60f25478397248392a8d99b22cda366a941715
rjrajujha <rjrajujha@outlook.com>
2024-03-29 08:16:43 +0530

    aligned

--- index.php
+++ index.php
@@ -11,23 +11,25 @@
 </head>
 
 <body>
-    <?php include 'includes/header.php'; ?>
+    <div class="d-flex flex-column justify-content-between">
+        <?php include 'includes/header.php'; ?>
 
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col">
-                <h2 class="mb-4">About Me</h2>
-                <p class="lead">
-                    As a software developer, I specialize in JavaScript with a focus on Node.js and its associated frameworks and libraries such as Express.js, React.js, and Next.js. I also have proficiency in Python, primarily for solving algorithmic challenges and data structures. Although my expertise lies in backend and frontend development, I also possess a working knowledge of PHP.
-                </p>
-                <p>
-                    Throughout my career, I've gained valuable experience working with various databases, including MongoDB and MySQL. I'm passionate about creating efficient and scalable solutions, and I thrive in dynamic and collaborative environments where I can continuously learn and grow.
-                </p>
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col">
+                    <h2 class="mb-4">About Me</h2>
+                    <p class="lead">
+                        As a software developer, I specialize in JavaScript with a focus on Node.js and its associated frameworks and libraries such as Express.js, React.js, and Next.js. I also have proficiency in Python, primarily for solving algorithmic challenges and data structures. Although my expertise lies in backend and frontend development, I also possess a working knowledge of PHP.
+                    </p>
+                    <p>
+                        Throughout my career, I've gained valuable experience working with various databases, including MongoDB and MySQL. I'm passionate about creating efficient and scalable solutions, and I thrive in dynamic and collaborative environments where I can continuously learn and grow.
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
 
-    <?php include 'includes/footer.php'; ?>
+        <?php include 'includes/footer.php'; ?>
+    </div>
     <!-- Bootstrap JS and dependencies -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>

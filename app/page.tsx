c4ee8dbf3e82de641e264f305e889aa526ac42b4
rjrajujha <rjrajujha@outlook.com>
2024-07-05 10:40:19 +0530

    navlink removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
 import Hero from '@/components/Hero';
-import { FloatingNav } from '@/components/ui/floating-navbar';
-import { FaHome } from 'react-icons/fa';
-import Grid from '../components/Grid'
+// import { FloatingNav } from '@/components/ui/floating-navbar';
+// import { FaHome } from 'react-icons/fa';
+// import Grid from '../components/Grid'
 
 const Page = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className='max-w-7xl w-full'>
-        <FloatingNav
-          className=''
-          navItems={[
-            { name: 'Home', link: '/', icon: <FaHome /> }
-          ]}
-        />
         <Hero />
       </div>
     </main>
